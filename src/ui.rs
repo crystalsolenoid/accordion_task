f0e0fee376dd0a1a47e3ab780cfe648f2b9d5d39
Quinten Konyn <qkonyn@gmail.com>
2024-01-16 17:29:03 -0800

    Render a table instead of a list

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,55 @@
 use ratatui::{
-  prelude::{Alignment, Frame},
+  prelude::{Alignment, Frame, Constraint},
   style::{Color, Style, Modifier},
-  widgets::{List, ListState, ListDirection, Block, BorderType, Borders, Paragraph},
+  widgets::{*},
 };
 
-use crate::app::App;
+use crate::app::{self, App};
 
 pub fn render(app: &mut App, f: &mut Frame) {
+//  render_list(app, f);
+  render_table(app, f);
+}
+
+fn render_table(app: &mut App, f: &mut Frame) {
+  let rows = [
+    generate_task_row(&app.tasks.items[0]),
+    generate_task_row(&app.tasks.items[1]),
+    generate_task_row(&app.tasks.items[2]),
+  ];
+  let widths = [
+      Constraint::Length(5),
+      Constraint::Length(25),
+      Constraint::Length(15),
+  ];
+  let table = Table::new(rows, widths)
+    .column_spacing(1)
+    .style(Style::new().fg(Color::Yellow))
+    .header(
+      Row::new(vec!["", "Task", "Dur"])
+        .style(Style::new().add_modifier(Modifier::BOLD))
+        .bottom_margin(1),
+    )
+    .block(Block::default()
+      .title("Routine")
+      .title_alignment(Alignment::Center)
+      .borders(Borders::ALL)
+      .padding(Padding::new(2, 2, 1, 1))
+    )
+    .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
+    .highlight_symbol(">> ");
+  f.render_stateful_widget(
+    table,
+    f.size(),
+    &mut TableState::default().with_selected(Some(0))
+  );
+}
+
+fn generate_task_row(task: &app::Task) -> Row {
+  Row::new(vec!["[ ]", "Row12", "Row13"])
+}
+
+fn render_list(app: &mut App, f: &mut Frame) {
   let items = ["Item 1", "Item 2", "Item 3"];
   f.render_stateful_widget(
   List::new(items)
@@ -17,5 +60,5 @@ pub fn render(app: &mut App, f: &mut Frame) {
     .repeat_highlight_symbol(true)
     .direction(ListDirection::TopToBottom),
   f.size(), &mut app.tasks.state
-  )
+  );
 }
