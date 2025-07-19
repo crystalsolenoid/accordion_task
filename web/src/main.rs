1453f5cf71b508ecf4a9039055365d5617f9e911
Quinten Konyn <qkonyn@gmail.com>
2025-07-19 01:12:52 -0700

    Add Durations

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,13 +1,34 @@
 use leptos::prelude::*;
+use std::time::Duration;
 
-use accordion_core::routine::Task;
+use accordion_core::routine::{Routine, Task};
+
+#[component]
+fn Routine(
+    name: RwSignal<String>,
+    elapsed: RwSignal<Duration>,
+    duration: RwSignal<Duration>,
+) -> impl IntoView {
+    view! {
+        {{ name }}: {{ elapsed.get().as_secs() }} / {{ duration.get().as_secs() }}
+        <progress value=move || elapsed.get().as_secs() as f64 / duration.get().as_secs() as f64 />
+    }
+}
 
 #[component]
 fn App() -> impl IntoView {
+    let mut list = Routine::default();
+    list.push(Task::new("a", 120));
+    list.push(Task::new("b", 60));
+
     let myTask = Task::new("wash dishes", 120);
+    let name = RwSignal::new("wash dishes".to_string());
+    let elapsed = RwSignal::new(Duration::from_secs(40));
+    let duration = RwSignal::new(Duration::from_secs(120));
 
     view! {
         <p>{{ myTask.name }}</p>
+        <Routine name=name elapsed=elapsed duration=duration/>
     }
 }
 
