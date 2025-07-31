6605210cdd22698cbdcf05195a8a2c6f309662ee
Quinten Konyn <qkonyn@gmail.com>
2025-07-30 22:31:34 -0700

    Leptosfmt

--- web/src/main.rs
+++ web/src/main.rs
@@ -19,9 +19,7 @@ use accordion_core::utils;
 fn Duration(
     #[prop(into)]
     value: Signal<Duration>) -> impl IntoView {
-    view! {
-        {{ move || utils::format_duration(value.get()) }}
-    }
+    view! { {{ move || utils::format_duration(value.get()) }} }
 }
 
 #[component]
@@ -30,9 +28,8 @@ fn RoutineTimer(
     routine: Field<Routine>,
 ) -> impl IntoView {
     view! {
-        <p>
-    <Duration value=routine.get().remaining() />
-        </p>
+        <p>Remaining: <Duration value=routine.get().remaining() /></p>
+        <p>Elapsed: <Duration value=routine.get().elapsed() /></p>
     }
 }
 
@@ -44,18 +41,24 @@ fn TaskListItem(
     view! {
         <h2>{{ move || task.name().get() }}</h2>
         <span>
-        "(" {{ move || match task.status().get() {
-                CompletionStatus::NotYet => "Incomplete",
-                CompletionStatus::Done => "Done",
-                CompletionStatus::Skipped => "Skipped",
-    } }} ")"
+            "("
+            {{
+                move || match task.status().get() {
+                    CompletionStatus::NotYet => "Incomplete",
+                    CompletionStatus::Done => "Done",
+                    CompletionStatus::Skipped => "Skipped",
+                }
+            }} ")"
         </span>
         <div>
-            <span> <Duration value=task.elapsed() />
-        </span>
-        <progress value=move || task.elapsed().get().as_secs() as f64 / task.duration().get().as_secs() as f64 />
-        <span>
-            <Duration value=task.duration() />
+            <span>
+                <Duration value=task.elapsed() />
+            </span>
+            <progress value=move || {
+                task.elapsed().get().as_secs() as f64 / task.duration().get().as_secs() as f64
+            } />
+            <span>
+                <Duration value=task.duration() />
             </span>
         </div>
     }
@@ -77,31 +80,37 @@ fn App() -> impl IntoView {
     );
 
     view! {
-    <RoutineTimer routine=data/>
-    <button
-        on:click=move |_| {data.write().toggle(active.get());}
-        >Complete Current</button>
-    <button
-        on:click=move |_| {data.write().skip(active.get());}
-        >Skip Current</button>
-    <ol class="routine">
-        <For
-            each=move || data.tasks().into_iter().enumerate()
-            key=|(_, task)| task.read().name.clone()
+        <RoutineTimer routine=data />
+        <button on:click=move |_| {
+            data.write().toggle(active.get());
+        }>Complete Current</button>
+        <button on:click=move |_| {
+            data.write().skip(active.get());
+        }>Skip Current</button>
+        <ol class="routine">
+            <For
+                each=move || data.tasks().into_iter().enumerate()
+                key=|(_, task)| task.read().name.clone()
                 children=move |(i, child)| {
-                view! {
-                    <li class="task">
-                    <label for=child.clone().name()>
-                    <TaskListItem task=child.clone()/>
-                    </label>
-                    <input type="radio" class="active-task" value=i id=child.name() name="active"
-                    prop:checked={i == 0}
-                    on:change=move |_| set_active.set(Some(i))/>
-                    </li>
+                    view! {
+                        <li class="task">
+                            <label for=child.clone().name()>
+                                <TaskListItem task=child.clone() />
+                            </label>
+                            <input
+                                type="radio"
+                                class="active-task"
+                                value=i
+                                id=child.name()
+                                name="active"
+                                prop:checked=i == 0
+                                on:change=move |_| set_active.set(Some(i))
+                            />
+                        </li>
+                    }
                 }
-            }
-        />
-    </ol>
+            />
+        </ol>
     }
 }
 
