0cef49e3bddf6217f9f1f3fe5d3fade125254e0f
Quinten Konyn <qkonyn@gmail.com>
2025-11-09 19:08:33 -0800

    Allow creating a new empty routine with a name
    A little buggy. You have to open and close it before it'll actually let
    you see your edits to it once it's created. Weird.

--- web/src/components/picker.rs
+++ web/src/components/picker.rs
@@ -1,8 +1,9 @@
 use accordion_core::routine::template::{RoutineTemplate, RoutineTemplateStoreFields};
 use gloo_storage::{LocalStorage, Storage};
-use leptos::prelude::*;
+use leptos::{html, prelude::*};
 use leptos_router::components::A;
 use reactive_stores::{Store, StoreFieldIterator};
+use web_sys::SubmitEvent;
 
 use crate::local_storage::{StoredRoutines, StoredRoutinesStoreFields};
 
@@ -10,6 +11,8 @@ use crate::local_storage::{StoredRoutines, StoredRoutinesStoreFields};
 pub fn Picker() -> impl IntoView {
     let routines: StoredRoutines = LocalStorage::get("stored-routines").unwrap_or_default();
     let routines_store: Store<StoredRoutines> = Store::new(routines);
+
+    let new_routine_name: NodeRef<html::Input> = NodeRef::new();
 
     Effect::new(move |_| {
         LocalStorage::set("stored-routines", routines_store.get());
@@ -19,23 +22,43 @@ pub fn Picker() -> impl IntoView {
 
     view! {
         "Pick a routine."
+        <form on:submit=move |ev: SubmitEvent| {
+            ev.prevent_default();
+            let name = new_routine_name.get()
+                .expect("<input> should be mounted")
+                .value();
+            routines_store.vec_field().write().push(RoutineTemplate::new(name, vec![]));
+        }>
+        "Name"
+            <input type="text"
+                node_ref=new_routine_name
+                required
+            />
+            <button>
+            "Create New Routine"
+            </button>
+        </form>
         <ul>
-        {move || routines_store.vec_field().iter_unkeyed().enumerate()
-            .map(|(i, routine)| view!{
+        <ForEnumerate
+            each=move || routines_store.vec_field().iter_unkeyed()
+            key=|routine| routine.name().get()
+            children=move |i, routine| {
+                view!{
                 <li>
-                <A href=move || "routine/".to_string()+&routine.name().get()>{{routine.name()}}</A>
+                <A href="routine/".to_string()+&routine.name().get()>{{routine.name()}}</A>
                 <button on:click = move |_| {
-                    let new_routine = routine.get();
+                    let old_routine = routine.get(); // TODO cloning broken
                     let new_name = routine.name().get() + "cloned";
-                    routines_store.vec_field().write().push(RoutineTemplate::new(new_name, vec![]));
+                    let mut new_routine = RoutineTemplate::new(new_name, vec![]);
+                    old_routine.tasks.iter().for_each(|task| new_routine.push(task.clone()));
+                    routines_store.vec_field().write().push(new_routine);
                 }>clone</button>
                 <button on:click = move |_| {
-                    set_last_delete.set(Some(routines_store.vec_field().write().remove(i)));
+                    set_last_delete.set(Some(routines_store.vec_field().write().remove(i.get())));
                 }>delete</button>
                 </li>
-            })
-            .collect_view()
-        }
+            }
+        }/>
         </ul>
         <Show
             when=move || {last_delete.get().is_some()}
